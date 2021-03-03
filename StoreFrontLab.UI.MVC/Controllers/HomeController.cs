68685a611d0936c1eccd2d3c5c3b0ca84c3cbe14
Sawyer Gobin <sawyergobin@outlook.com>
2021-03-03 10:45:27 -0600

    Add project files.

---
+++ StoreFrontLab.UI.MVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+
+namespace StoreFrontLab.UI.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        //[Authorize] Commented out until logins exits
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
+
+
