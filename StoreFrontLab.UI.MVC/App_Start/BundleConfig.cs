3818a6f4ff37acd6b1a435c751954687322ec4a8
sawyergobin <sawyergobin@outlook.com>
2021-04-07 15:15:21 -0500

    Bundled styles and scripts

--- StoreFrontLab.UI.MVC/App_Start/BundleConfig.cs
+++ StoreFrontLab.UI.MVC/App_Start/BundleConfig.cs
@@ -7,24 +7,27 @@ namespace StoreFrontLab.UI.MVC
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+           
+            bundles.Add(new ScriptBundle("~/Content/scripts").Include(
+                      "~/Content/js/jquery-3.3.1.min.js",
+                      "~/Content/js/bootstrap.min.js",
+                      "~/Content/js/jquery.nice-select.min.js",
+                      "~/Content/js/jquery-ui.min.js",
+                      "~/Content/js/jquery.slicknav.js",
+                      "~/Content/js/mixitup.min.js",
+                      "~/Content/js/owl.carousel.min.js",
+                      "~/Content/js/main.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/styles").Include(
+                      "~/Content/css/bootstrap.min.css",
+                      "~/Content/css/font-awesome.min.css",
+                      "~/Content/css/elegant-icons.css",
+                      "~/Content/css/nice-select.css",
+                      "~/Content/css/jquery-ui.min.css",
+                      "~/Content/css/owl.carousel.min.css",
+                      "~/Content/css/slicknav.min.css"));
         }
     }
 }
